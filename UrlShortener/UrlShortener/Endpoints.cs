cc5095a9d4c2726c4bfcf31fe1d9ac2fd442f45e
Jordan <jordanaaronbrown@gmail.com>
2026-01-30 14:16:24 +0000

    Add initial files
    Add an interface for future endpoints, and some stubs for grouping them to the `/url` route.

---
+++ UrlShortener/UrlShortener/Endpoints.cs
@@ -0,0 +1,33 @@
+﻿using UrlShortener.Interface;
+using UrlShortener.Url.Endpoints;
+
+namespace UrlShortener;
+
+public static class Endpoints
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        var endpoints = app.MapGroup("");
+        
+        endpoints.MapUrlEndpoints();
+    }
+    
+    private static void MapUrlEndpoints(this IEndpointRouteBuilder app)
+    {
+        var endpoints = app.MapGroup("/url")
+            .WithTags("Urls");
+        
+    }
+    
+    private static RouteGroupBuilder MapPublicGroup(this IEndpointRouteBuilder app, string? prefix = null)
+    {
+        return app.MapGroup(prefix ?? string.Empty)
+            .AllowAnonymous();
+    }
+    
+    private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) where TEndpoint : IEndpoint
+    {
+        TEndpoint.Map(app);
+        return app;
+    }
+}
---
+++ UrlShortener/UrlShortener/Program.cs
@@ -0,0 +1,8 @@
+using UrlShortener;
+using UrlShortener.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<UrlRepository>();
+var app = builder.Build();
+app.MapEndpoints();
+app.Run();
